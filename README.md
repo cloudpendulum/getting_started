10b9fb8c89c7fadd41f77aa6778aa481d63cbec6
Shivesh Kumar <shivesh.kumar@dfki.de>
2025-07-12 02:51:43 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Cloud Pendulum Tutorial
---
+++ misc.py
@@ -0,0 +1,30 @@
+import subprocess
+import wget
+from pathlib import Path
+
+def download_video(video_url):
+    input_path = wget.download(video_url, "videos")
+    output_path = Path(input_path).parent / (str(Path(input_path).stem) + '.mp4')
+    convert_flv_to_mp4(input_path, output_path)
+    return output_path
+
+
+def convert_flv_to_mp4(input_path, output_path):
+    """
+    Convert an FLV file to MP4 using FFmpeg.
+
+    :param input_path: Path to the input FLV file.
+    :param output_path: Path to the output MP4 file.
+    """
+    command = [
+        "ffmpeg",
+        "-i", input_path,    # Input file
+        "-c:v", "copy",      # Copy video stream
+        "-c:a", "copy",      # Copy audio stream
+        output_path          # Output file
+    ]
+    process = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if process.returncode == 0:
+        print(f"Conversion successful: {output_path}")
+    else:
+        print(f"Error during conversion: {process.stderr.decode()}")
